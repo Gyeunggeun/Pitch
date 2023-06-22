5a3c6a06ccb048ff2de235e4ed459cabeee84f4e
senseofurgency <kgh5654@naver.com>
2023-06-22 10:01:56 +0900

    새로운 브랜치

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcl
+import matplotlib.patches as mpt
+import seaborn as sns
 import plotly.express as px
 from streamlit_echarts import st_echarts
 from streamlit_extras.switch_page_button import switch_page
@@ -9,19 +14,28 @@ from streamlit_faker import get_streamlit_faker
 # streamlit faker
 fake = get_streamlit_faker(seed=42)
 
-# 부상자 csv 데이터프레임 !!
-# injury = pd.read_csv('injury.csv')
-# df1 = injury[['선수ID', '포지션', '출장경기수', '이닝', '투구수', '승리', '패배', '홀드', '세이브', 'ERA', '탈삼진', 'WHIP']].iloc[[0]]
-# df1 = df1.set_index('선수ID')
+# 현재부상투수현황 데이터프레임
+now_injured = pd.read_csv('now_injured.csv', encoding='cp949')
+# 부상x선수 중 부상 위험도 데이터프레임
+high = pd.read_csv('high.csv', encoding='cp949')
 
-# 경기일정 csv 데이터프레임 !!
-# date
+# 재활, 부상, 가능 나열하기 위한 리스트
+injury = ['가능','재활','부상']
+
+
 
 #일단 예시로 해당csv파일 활용
 df = pd.read_csv('lgpitch.csv')
 df1 = df[['선수ID', '포지션', '출장경기수', '이닝', '투구수', '승리', '패배', '홀드', '세이브', 'ERA', '탈삼진', 'WHIP']].iloc[[0]]
 df1 = df1.set_index('선수ID')
 
+# injured_list.csv 파일
+Injured_List = pd.read_csv('Injured_List.csv')
+
+# injured_list3.csv 파일
+Injured_List3 = pd.read_csv('Injured_List3.csv',encoding='cp949' )
+Injured_List3 = Injured_List3.groupby(['부상명'])['선수'].count()
+Injured_List3 = Injured_List3.sort_values(ascending=False)
 
 # -------------------- ▼ 필요 변수 생성 코딩 Start ▼ --------------------
 
@@ -50,20 +64,107 @@ if want_to_contribute1:
 col11, col12 = st.columns(2)
 with col11:
     st.subheader("요약")
-    st.write(f"시즌 총 부상빈도 : {df1['출장경기수'][0]}") # st.write('팀부상빈도:', injury[부상].sum)
-    st.write(f"시즌 총 누적부상일수 : {df1['이닝'][0]}")
     #fake.dataframe()
-    #부상투수통계 도넛차트
     
     col111, col112 = st.columns(2)
     with col111:
+        col111.metric('시즌 총 부상빈도', '7 회', '작년 대비 2 회')
         # 현재 투수 부상 현황
-        fig = px.pie(df1, values="이닝", names = "포지션", title="현재 투수 부상 현황", hole=.7)
-        fig.update_traces(textposition='inside', textinfo='percent+label+value')
+        fig = px.pie(now_injured.sort_values(by = '출전여부', key=lambda s: s.map({k: i for i, k in enumerate(injury)})), values="값", names = "출전여부", title="현재 투수 부상 현황", hole=.7, 
+                     color = '출전여부', color_discrete_map={'부상':'#df839b', '재활':'#8e8e8d', '가능':'#f6f6f6'})
+        fig.update_traces(textposition='outside', textinfo='percent+label+value', textfont_size=10)
         fig.update_layout(font=dict(size=16))
+        fig.update_layout(width=250,height=300)
+        fig.update(layout_showlegend=False)
         st.plotly_chart(fig)
         
-        # 반원 차트1
+
+    #부상고위험투수통계 도넛차트
+    
+    with col112:
+        col112.metric('시즌 총 누적부상일수', '125 일', ' 작년대비 20일')
+        
+        # 현재 팀 투수 부상 누적일수
+        fig2 = px.pie(high, values="값", names = "부상위험", title="현재 부상 고위험 투수 통계", hole=.7
+                      , color = '부상위험', color_discrete_map={'고위험':'#BE0737', '보통':'#ededed'})
+        fig2.update_traces(textposition='outside', textinfo='percent+label+value', textfont_size=10)
+        fig2.update_layout(font=dict(size=16))
+        fig2.update_layout(width=250,height=300)
+        fig2.update(layout_showlegend=False)
+        st.plotly_chart(fig2)
+        
+
+# -------------------- ▲ 요약 End ▲ --------------------
+
+## -------------------- ▼ 경기일정 START ▼ --------------------
+
+with col12:
+    st.subheader("경기일정")
+    st.dataframe(df)  # 경기일정 data프레임이 들어가야함
+
+# --------------------- ▲ 경기일정 End ▲ --------------------
+
+
+## -------------------- ▼ 부상통계 START ▼ --------------------
+
+st.subheader("부상통계")
+st.write("\n")  # additional space
+col21, col22, col23 = st.columns([0.8, 0.1, 1.0]) # st.columns([0.1, 0.3, 0.1, 0.3])
+
+# 팀부상종류 통계
+with col21:
+    st.text('2023시즌 팀 부상 종류')
+    st.dataframe(Injured_List3)
+    
+# 팀부상선수 한명씩 나열    
+# 선수 1
+with col23:
+    st.text('최근 부상자')
+    col231, col232, col233, col234, col235= st.columns([0.4, 0.05, 0.4, 0.05, 0.4])
+    with col231:
+        st.image('https://raw.githubusercontent.com/Gyeunggeun/Pitch/main/pitch_images/%EC%9D%B4%EC%A0%95%EC%9A%A9.png', width=200)
+        st.markdown("""<br>
+         <div style="text-align: center;">
+             <h3 style="margin-top: 15px;">이정용</h3>
+         </div>
+         """, unsafe_allow_html=True)
+        
+        st.text('부상부위: tommy john surgury') # 1. 파일에 부상정보가 없음(가라데이터 필요) 2. 지금은 선수 부상부위가 중복으로 들어간게 있음      
+        st.text(f"부상발생일: {Injured_List['날짜'][0]}") # 해당 선수의 부상일자로 변경해야함
+        st.text("예상 복귀일: D-7")
+        
+# 선수 2    
+    with col233:
+        st.image('https://raw.githubusercontent.com/Gyeunggeun/Pitch/main/pitch_images/%EA%B3%A0%EC%9A%B0%EC%84%9D.png', width=200)
+        st.markdown("""<br>
+         <div style="text-align: center;">
+             <h3 style="margin-top: 15px;">고우석</h3>
+         </div>
+         """, unsafe_allow_html=True)
+        
+        st.text('부상부위: tommy john surgury')           
+        st.text(f"부상 발생일: {Injured_List['날짜'][1]}") 
+        st.text("예상 복귀일: D-7")
+ 
+# 선수 3        
+    with col235:
+        st.image('https://raw.githubusercontent.com/Gyeunggeun/Pitch/main/pitch_images/%EC%9D%B4%EB%AF%BC%ED%98%B8.png', width=200)
+        st.markdown("""<br>
+         <div style="text-align: center;">
+             <h3 style="margin-top: 15px;">이민호</h3>
+         </div>
+         """, unsafe_allow_html=True)
+        
+        st.text('부상부위: tommy john surgury')           
+        st.text(f"부상발생일: {Injured_List['날짜'][0]}") 
+        st.text("예상 복귀일: D-3")
+    
+    
+    
+    
+    
+    # <혹시 몰라, 안버린 코드>
+    # 1.  반원 차트
         # option = {
         #     "tooltip": {
         #         "formatter": '{a} <br/>{b} : {c}%'
@@ -115,171 +216,3 @@ with col11:
         #     }]
         # };
         # st_echarts(options=option, key="1")
-
-    #부상고위험투수통계 도넛차트
-    
-    with col112:
-        # 현재 팀 투수 부상 누적일수
-        fig2 = px.pie(df1, values="출장경기수", names = "포지션", title="현재 부상 고위험 투수 통계", hole=.7)
-        fig2.update_traces(textposition='inside', textinfo='percent+label+value')
-        fig2.update_layout(font=dict(size=16))
-        st.plotly_chart(fig2)
-        
-        # 반원 차트1
-        # option1 = {
-        #     "tooltip": {
-        #         "formatter": '{a} <br/>{b} : {c}%'
-        #     },
-        #     "series": [{
-        #         "type": 'gauge',
-        #         "startAngle": 180,
-        #         "endAngle": 0,
-        #         "progress": {
-        #             "show": "true"
-        #         },
-        #         "radius":'100%', 
-
-        #             "itemStyle": {
-        #             "color": '#58D9F9',
-        #             "shadowColor": 'rgba(0,138,255,0.45)',
-        #             "shadowBlur": 10,
-        #             "shadowOffsetX": 2,
-        #             "shadowOffsetY": 2,
-        #             "radius": '55%',
-        #         },
-        #         "progress": {
-        #             "show": "true",
-        #             "roundCap": "true",
-        #             "width": 15
-        #         },
-        #         "pointer": {
-        #             "length": '60%',
-        #             "width": 8,
-        #             "offsetCenter": [0, '5%']
-        #         },
-        #         "detail": {
-        #             "valueAnimation": "true",
-        #             "formatter": '{value}%',
-        #             "backgroundColor": '#58D9F9',
-        #             "borderColor": '#999',
-        #             "borderWidth": 4,
-        #             "width": '60%',
-        #             "lineHeight": 20,
-        #             "height": 20,
-        #             "borderRadius": 188,
-        #             "offsetCenter": [0, '40%'],
-        #             "valueAnimation": "true",
-        #         },
-        #         "data": [{
-        #             "value": 66.66
-        #         }]
-        #     }]
-        # };
-        # st_echarts(options=option1, key="2")
-
-# -------------------- ▲ 요약 End ▲ --------------------
-
-## -------------------- ▼ 경기일정 START ▼ --------------------
-
-with col12:
-    st.subheader("경기일정")
-    st.dataframe(df)  # 경기일정 data프레임이 들어가야함
-
-# --------------------- ▲ 경기일정 End ▲ --------------------
-
-st.subheader("부상통계")
-col21, col22, col23, col24, col25, col26, col27 = st.columns(2)
-
-
-# # 선수 이미지 URL
-# players = { 
-#     '강효종': 'https://raw.githubusercontent.com/Gyeunggeun/Pitch/6a00464c37f059ac3b52898fabd77bad8e7b36f3/pitch_images/%EA%B0%95%ED%9A%A8%EC%A2%85.png',
-#     '함덕주': 'https://raw.githubusercontent.com/Gyeunggeun/Pitch/main/pitch_images/%ED%95%A8%EB%8D%95%EC%A3%BC.png'
-# }
-
-# # 사이드바
-# pages = ['선수 목록'] + list(players.keys())
-# selected_page = st.sidebar.selectbox('페이지', pages)
-
-# # 선수 목록 페이지 구성
-# if selected_page == '선수 목록':
-#     st.title('선수 목록')
-#     player_list = list(players.keys())
-#     for i in range(0, len(player_list), 5):  # 5*7 로 총 35명의 선수 이미지 표현
-#         columns = st.columns(5)
-#         for j in range(5):
-#             if i + j < len(player_list):
-#                 player = player_list[i + j]
-#                 with columns[j]:
-#                     image_url = players[player]
-#                     components.html(f'''
-#                         <center>
-#                             <img src="{image_url}" width="100%">
-#                             <p style="margin-top: 0px; color: white; font-size: 20px; font-weight: bold">{player}</p>
-#                         </center>
-#                     ''', height=200)
-# else:
-#     st.title(f'{selected_page}')
-#     # 선수의 세부 페이지에서 보여줄 정보
-#     if selected_page == '강효종':
-#         tab1, tab2, tab3 = st.tabs(['선수 프로필', '부상분석', '부상위험요인'])
-#         with tab1:
-#             col301, col302 = st.columns(2)
-#             with col301:
-#                 st.image('https://raw.githubusercontent.com/Gyeunggeun/Pitch/main/pitch_images/%EA%B0%95%ED%9A%A8%EC%A2%85.png', width=300)
-#             with col302:
-#                 st.subheader("선수 기본 프로필")
-#                 st.text("이름: 강효종")
-#                 st.text("포지션: 투수")
-#                 st.text("팀: LG 트윈스")
-#                 st.text("생년월일: 2002년 10월 14일")
-#                 st.text("신장/체중: 184cm/86kg")
-#             st.write('2023시즌') # 볼드체로
-#             st.dataframe(df1, width=1000)
-#             st.markdown("   ")
-#             st.subheader("최근 부상 이력")
-#             st.text("5월 27일 Tommy john surgery (23일 전)") # 이부분 표로?? 아님 데이터프레임?? 
-#             col303, col304 = st.columns(2)
-#             with col303 :
-#                 #st.write('2023시즌')
-#                 #st.markdown("   ")
-#                 #st.subheader("최근 부상 이력")
-#                 #st.text("5월 27일 Tommy john surgery (23일 전)") # 이부분 표로?? 아님 데이터프레임?? 
-#                 st.image('0619/부상이력히트맵.png', width=500)
-#             with col304:
-#                 #st.markdown("    ") # 줄바꿈 여러번 추가
-#                 st.image('0619/예측히트맵.png', width=500)
-
-#         with tab2:
-#             st.subheader('투구 분석')
-#             # st.image('투구별 어깨,팔꿈치 부상위험도 차트 이미지 삽입')
-#             option = st.selectbox('투구를 선택하세요',
-#                          ['1구', '2구', '3구', '4구', '5구', '6구', '7구', '8구', '9구', '10구','11구', '12구', '13구', '14구', '15구', '16구', '17구', '18구', '19구', '20구'])
-#             st.write('선택 옵션:', option)
-#             if option == '1구':
-#                 col401, col402 = st.columns(2)
-#                 with col401:
-#                     st.video('https://youtu.be/f-tq3W2HvT8') # 출처 필요 -> 세부 페이지에
-#                 with col402:
-#                     st.video('https://youtu.be/8s-ZllEX4Zk')
-#             elif option == '2구':
-#                 col403, col404 = st.columns(2)
-#                 with col403:
-#                     st.image('0619/스켈레톤.png')
-#                 with col404:
-#                     st.image('0619/원본.png')
-#             # 여따가 들어갈 표, 시각화툴 필요함
-#             #이 선수의 팔 부상확률이 몇 프로 
-#             #유사 투구를 하는 선수 링크(?)
-#             st.markdown('') # 예시, 대체가능
-#         with tab3:
-#             st.subheader('부상위험요인')
-#             col501, col502, col503 = st.columns(3)
-#             with col501:
-#                 st.write('부상위험요인1')
-#     elif selected_page == '고우석':
-#         st.write('고우석 상세정보')
-#         # 고우석 상세정보 코드 여기에
-#     # 기타 선수들에 대한 코드는 elif를 이용하여 추가
-
-
