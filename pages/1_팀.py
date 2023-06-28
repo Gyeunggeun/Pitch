39151e614605730576c8b91bc8ee94ea7baa58cc
Gyeunggeun Lee <askwhy96@ajou.ac.kr>
2023-06-29 06:17:21 +0900

    로고 파일명 변경

--- pages/1_팀.py
+++ pages/1_팀.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_icon="⚾️",
     layout="wide",
     initial_sidebar_state="expanded")
-add_logo("_솔루션로고\\로고확장4.png", height=370)
+add_logo("solutionlogo\\final.png", height=370)
 
 st.title('팀')
 
